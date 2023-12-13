50440c9d33ce72840206a8494f6fb04cb8ccd2bf
tushar-454 <imtushar454@gmail.com>
2023-12-13 17:56:56 +0600

    card item delete funtion added and using tanstack refetch function

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,17 +1,45 @@
 import { useQuery } from '@tanstack/react-query';
+import swal from 'sweetalert';
 import useAuth from '../../Hook/useAuth';
 import useAxios from '../../Hook/useAxios';
+import deleteIcon from '../../assets/delete.png';
 
 const Cart = () => {
   const axios = useAxios();
   const { user } = useAuth();
-  const { data: carts, isLoading } = useQuery({
+  const {
+    data: carts,
+    isLoading,
+    refetch,
+  } = useQuery({
     queryKey: ['cartItem'],
     queryFn: async () => {
       const res = await axios.get(`/cart/${user.email}`);
       return res.data;
     },
   });
+  const handleDeleteCart = (id) => {
+    swal({
+      title: 'Are you sure for delete ?',
+      text: '',
+      icon: 'warning',
+      buttons: true,
+      dangerMode: true,
+    }).then((willDelete) => {
+      if (willDelete) {
+        axios.delete(`/cart/${id}`).then((res) => {
+          if (res.data.deletedCount) {
+            refetch();
+            swal('Cart item has been deleted!', {
+              icon: 'success',
+            });
+          }
+        });
+      } else {
+        swal('Your cart safe now !');
+      }
+    });
+  };
   return (
     <seciton>
       <div>
@@ -24,6 +52,7 @@ const Cart = () => {
                 <td className='no-wrap border p-3 font-bold'>Price</td>
                 <td className='no-wrap border p-3 font-bold'>Date</td>
                 <td className='no-wrap border p-3 font-bold'>Status</td>
+                <td className='no-wrap border p-3 font-bold'>Delete</td>
               </tr>
             </thead>
             <tbody>
@@ -38,6 +67,14 @@ const Cart = () => {
                     </td>
                     <td className='no-wrap border p-3 tex'>{cart.date}</td>
                     <td className='no-wrap border p-3 tex'>{cart.status}</td>
+                    <td className='no-wrap border p-3 tex'>
+                      <img
+                        src={deleteIcon}
+                        alt='delete'
+                        className='w-5 cursor-pointer'
+                        onClick={() => handleDeleteCart(cart._id)}
+                      />
+                    </td>
                   </tr>
                 ))}
             </tbody>
